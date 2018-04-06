97156e3f0b184c7d8e84c87aa24ae45fec6b7979
David Kironde <david.kironde@andela.com>
2018-04-06 17:12:16 +0300

    format js documents

--- controllers/updateuser.js
+++ controllers/updateuser.js
@@ -1,21 +1,21 @@
 const User = require('../models/userSchema');
 
 const updateUserHandler = (req, res) => {
-  const {name, nickname, } = req.body;
+  const { name, nickname, } = req.body;
   const { id } = req.params
-User.findOneAndUpdate({
-  _id: id
-}, {
-  $set:{
-    name:name,
-    nickname: nickname
-  }
-}, null, (err, updateduser) => {
-  if(err){
-    res.status(500).json('Error server Error')
-  }
-  res.json(updateduser)
-})
+  User.findOneAndUpdate({
+    _id: id
+  }, {
+      $set: {
+        name: name,
+        nickname: nickname
+      }
+    }, null, (err, updateduser) => {
+      if (err) {
+        res.status(500).json('Error server Error')
+      }
+      res.json(updateduser)
+    })
 }
 
 module.exports = {
--- models/userSchema.js
+++ models/userSchema.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name:{
+  name: {
     type: String, default: ''
   },
-  nickname:{
-    type: String, default: ''}
+  nickname: {
+    type: String, default: ''
+  }
 });
 
-module.exports= mongoose.model('Users', userSchema)
+module.exports = mongoose.model('Users', userSchema)
