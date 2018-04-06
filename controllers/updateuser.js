64bb1fc019296d725c0ccf3c600961034662e3f3
David Kironde <david.kironde@andela.com>
2018-04-06 16:56:48 +0300

    add update user endpoint

--- controllers/updateuser.js
+++ controllers/updateuser.js
@@ -1,13 +1,20 @@
-const Users = require('../models/userSchema');
-
+const User = require('../models/userSchema');
 
 const updateUserHandler = (req, res) => {
-const user = new User;
-user.find({}, (err, users)=>{
+  const {name, nickname, } = req.body;
+  const { id } = req.params
+User.findOneAndUpdate({
+  _id: id
+}, {
+  $set:{
+    name:name,
+    nickname: nickname
+  }
+}, null, (err, updateduser) => {
   if(err){
     res.status(500).json('Error server Error')
   }
-  res.json('users')
+  res.json(updateduser)
 })
 }
 
